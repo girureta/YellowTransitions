d44a009170c5e22742aa1f9a2fbda5968ef5caa9
Guillermo I <guillermo.iruretagoyena@gmail.com>
2019-06-12 23:58:47 +0200

    Adds a new class for handling the TransitionB effect. Adds a public instance of each transition effect. Adds the public enum 'Transition' which we use to indicate what effect to use. Keeps a protected TransitionEffect which points to the chosen one given the value in 'transition'. Makes TransitionEffect's 'Init' return a boolean to indicate if it is properly initialized.

--- Scripts/YellowTransitionEffect.cs
+++ Scripts/YellowTransitionEffect.cs
@@ -6,31 +6,73 @@ using UnityEngine;
 public class YellowTransitionEffect : MonoBehaviour
 {
     public TransitionAEffect transitionAShader;
+    public TransitionBEffect transitionBShader;
+    protected TransitionEffect transitionShader;
+
+    public enum Transition
+    {
+        TransitionA,
+        TransitionB
+    }
+
+    public Transition transition = Transition.TransitionA;
+    protected Transition m_Transition = Transition.TransitionA;
 
     void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
-        transitionAShader.OnRenderImage(source, destination);
+        if (transitionShader == null || transition != m_Transition)
+        {
+            m_Transition = transition;
+            transitionShader = GetTransitionEffect(m_Transition);
+        }
+        transitionShader.OnRenderImage(source, destination);
+    }
+
+    protected TransitionEffect GetTransitionEffect(Transition transition)
+    {
+        TransitionEffect effect = null;
+
+        switch (transition)
+        {
+            case Transition.TransitionA:
+                effect = transitionAShader;
+                break;
+            case Transition.TransitionB:
+                effect = transitionBShader;
+                break;
+            default:
+                break;
+        }
+
+        return effect;
     }
 
     public abstract class TransitionEffect
     {
         public Shader shader;
         protected Material material;
-        protected void Init()
+        protected bool Init()
         {
+            if (shader == null)
+                return false;
+
             if (material == null)
             {
                 material = new Material(shader);
             }
+
+            return true;
         }
 
         public abstract void Update();
 
         public void OnRenderImage(RenderTexture source, RenderTexture destination)
         {
-            Init();
-            Update();
-            Graphics.Blit(source, destination, material);
+            if (Init())
+            {
+                Update();
+                Graphics.Blit(source, destination, material);
+            }
         }
     }
 
@@ -47,6 +89,23 @@ public class YellowTransitionEffect : MonoBehaviour
         public override void Update()
         {
             material.SetFloat(AngleProperty, -angle);
+            material.SetFloat(FactorProperty, factor);
+        }
+    }
+
+    [System.Serializable]
+    public class TransitionBEffect : TransitionEffect
+    {
+        [Range(0.0f, 1.0f)]
+        public float progress = 0.0f;
+        public float factor = 18.75f;
+
+        protected const string ProgressProperty = "_Progress";
+        protected const string FactorProperty = "_Factor";
+
+        public override void Update()
+        {
+            material.SetFloat(ProgressProperty, progress);
             material.SetFloat(FactorProperty, factor);
         }
     }
